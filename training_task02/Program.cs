d96e2b9837c7046a1cd3a64010d401fbf9e4c841
Yaroslav <tarasdmb2008@mail.ru>
2023-02-18 10:09:24 +0500

    three task

--- training_task02/Program.cs
+++ training_task02/Program.cs
@@ -3,29 +3,18 @@
 // 78 -> третьей цифры нет
 // 32679 -> 6
 
-System.Console.WriteLine("Enter the number: ");
+
+System.Console.Write("Enter the number: ");
 int number = Convert.ToInt32(Console.ReadLine());
-if (number > 99 && number < 1000)
+if (number > 99)
 {
-    int threeN = number % 10;
-    System.Console.WriteLine(threeN);
+    while (number > 999)
+    {
+        number = number / 10;
+    }
+    System.Console.WriteLine(number % 10);
 }
-else if (number > 999 && number < 10000)
-{
-    int threeN = number / 10;
-    System.Console.WriteLine(threeN % 10);
-}
-else if (number > 9999 && number < 100000)
-{
-    int threeN = number / 100;
-    System.Console.WriteLine(threeN % 10);
-}
-else if (number > 99999 && number < 1000000)
-{
-    int threeN = number / 1000;
-    System.Console.WriteLine(threeN % 10);
-}
-if (number < 99)
+else
 {
     System.Console.WriteLine("There is no three-digit.");
 }
---
+++ training_task03/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+System.Console.WriteLine("Enter the day number of the week: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number <= 5)
+{
+    System.Console.WriteLine("No. This is a work day.");
+}
+else if (number <= 7)
+{
+     System.Console.WriteLine("Yes. This day is a day of.");
+}
+else 
+{
+    System.Console.WriteLine("It's not the day of the week.");
+}
